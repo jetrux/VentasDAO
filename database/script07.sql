3c932b1d452f202e459f970465650fac867b1554
Jesus Gaston Kuti <32679327+jetrux@users.noreply.github.com>
2020-11-24 16:44:00 -0300

    Update script07.sql

--- database/script07.sql
+++ database/script07.sql
@@ -6,8 +6,9 @@ CREATE TABLE public.facturas
     cliente_id integer NOT NULL,
     formas_pago_id integer NOT NULL,
     numero_factura serial NOT NULL,
-	fecha date NOT NULL,
-    observacion character varying(150),
+    fecha date NOT NULL,
+    observacion character varying(150) NOT NULL,
+    total double precision NOT NULL,
     CONSTRAINT pk_id_facturas PRIMARY KEY (id),
     CONSTRAINT fk_producto_id FOREIGN KEY (producto_id)
         REFERENCES public.productos (id) MATCH SIMPLE
@@ -19,7 +20,7 @@ CREATE TABLE public.facturas
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID,
-	CONSTRAINT fk_formas_pago_id FOREIGN KEY (formas_pago_id)
+    CONSTRAINT fk_formas_pago_id FOREIGN KEY (formas_pago_id)
         REFERENCES public.formas_pago (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
